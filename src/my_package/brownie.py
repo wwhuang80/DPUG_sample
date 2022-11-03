949b8ee0c09b546a3e4f63c58494e540442ef6c3
wwhuang <wwhuang@ucdavis.edu>
2022-11-03 15:46:33 -0700

    - additional type-checking input validation.

--- src/my_package/brownie.py
+++ src/my_package/brownie.py
@@ -1,5 +1,14 @@
 def brownie(number: int):
-    if number < 0:
-        raise ValueError(f"The number of brownies must be greater than or equal to zero.")
+    """
+
+    :param number:
+    :return:
+    """
+    if not isinstance(number, int):
+        raise TypeError(f"The number input must be an integer.")
+    elif number < 0:
+        raise ValueError(
+            f"The number of brownies must be greater than or equal to zero."
+        )
     suffix = "s" if number > 1 else ""
-    return f"{str(int)} brownie{suffix}!"
+    return f"{str(number)} brownie{suffix}!"
